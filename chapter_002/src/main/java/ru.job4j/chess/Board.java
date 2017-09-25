7906db05b913596a0eda3c4388392518cddab005
yaegorko <yaegorko@gmail.com>
2017-09-25 22:07:26 +0400

    Изменил метод move, написал коментарии как я вижу задание. All green.

--- chapter_002/src/main/java/ru.job4j/chess/Board.java
+++ chapter_002/src/main/java/ru.job4j/chess/Board.java
@@ -31,23 +31,24 @@ public class Board {
     boolean move(Cell source, Cell dist) throws ImpossibleMoveException, OccupiedWayException, FigureNotFoundException, CloneNotSupportedException {
 
         for (int i = 0; i < figures.length; i++) {
-            if (source.equals(this.figures[i].getPosition())) {
-                Cell[] way = figures[i].way(dist);
-                    for (int n = 0; n < way.length; n++) {
-                        for (int j = 0; j < figures.length; j++) {
-                            if (way[n].equals(figures[j].getPosition())) {
-                                throw new OccupiedWayException("Путь занят");
+            if (source.equals(this.figures[i].getPosition())) { //проверяю есть ли на source фигура из массива Figures
+                Cell[] way = figures[i].way(dist); //если есть вызываю метод way для фигуры, он вернет либо путь, либо исключение ImpossibleMoveException
+                    for (int n = 0; n < way.length; n++) {          // на всем пути фигуры полученном из way
+                        for (int j = 0; j < figures.length; j++) {  // проверяем наличие всех других фигур из Figures
+                            if (way[n].equals(figures[j].getPosition())) { // проверяем позицию фигур из Figures на пути фигуры стоявшей на source
+                                throw new OccupiedWayException("Путь занят"); // если совпало бросаем OccupiedWayException
                             }
                         }
                     }
-                    figures[i] = figures[i].clone(dist);
-                    return true;
+                    figures[i] = figures[i].clone(dist); // если все ок, переписываем положение фигуры стоявшей на source, новым объектом
+                    return true; //цель достигнута, заканчиваем.
+                } else {
+                    throw new FigureNotFoundException("Нет фигуры на стартовых координатах"); //если нет фигуры на source бросаем FigureNotFoundException
                 }
             }
 
-        throw new FigureNotFoundException("Нет фигуры на стартовых координатах");
+        return false; //Сюда не должны доходить.
     }
-
 }
 
 
