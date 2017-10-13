b061551a37400ceb332f41364558d34ec15eaafc
yaegorko <yaegorko@gmail.com>
2017-10-13 22:02:37 +0400

    Дополнительное тестовое задание.Сортировка справочника подразделен.v1.0

--- chapter_003/src/main/java/ru/job4j/sortunits/SortUnits.java
+++ chapter_003/src/main/java/ru/job4j/sortunits/SortUnits.java
@@ -1,10 +1,23 @@
 package ru.job4j.sortunits;
 
-import java.util.*;
+import org.apache.commons.lang3.ArrayUtils;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.TreeSet;
+
+/**
+ * Класс сортирующий справочник подразделений.
+ */
 public class SortUnits {
 
-    public String[] parseUnits(String[] unsorted) {
+    /**
+     * Парсим список и выносим неявно указанные подразделения в отдельную строку.
+     * Побочный эффект подразделения сортируются по возрастанию ( задача 1)
+     * @param unsorted несортированный массив без некоторых явно указанных подразделений.
+     * @return сортированный по возрастанию массив со всеми подразделениями.
+     */
+    public String[] sortAscending(String[] unsorted) {
 
         TreeSet<String> allUnits = new TreeSet<>();
 
@@ -22,7 +35,6 @@ public class SortUnits {
                 } else {
                     allUnits.add(units);
                     startSubstring = 0;
-
                     break;
                 }
             }
@@ -33,17 +45,43 @@ public class SortUnits {
         return sortedAscending;
     }
 
-    public String[] sortAscending(String[] unsorted) {
-
-        String[] sorted = parseUnits(unsorted);
-
-        return sorted;
-    }
-
-
+    /**
+     * Сортируем массив по убыванию.
+     * @param unsorted несортированный массив без некоторых явно указанных подразделений.
+     * @return сортированный по убыванию массив со всеми подразделениями.
+     */
     public String[] sortDescendingly(String[] unsorted) {
-        String[] sorted = parseUnits(unsorted);
 
+        Comparator comparator = (Comparator<String>) (o1, o2) -> {
+            if (o1.length() < o2.length()) {
+                return -1;
+            } else if (o1.length() == o2.length()) {
+                return o2.compareTo(o1);
+            } else if (o1.length() > o2.length()) {
+                return 1;
+            }
+            return 0;
+        };
+
+        String[] sorted = sortAscending(unsorted);
+
+        int k1Length = 0;
+
+        for (String s: sorted) {
+            if (s.charAt(1) == 49) {
+                k1Length++;
+            } else {
+                break;
+            }
+        }
+
+        String[] k1 = Arrays.copyOf(sorted, k1Length);
+        String[] k2 = Arrays.copyOfRange(sorted, k1Length, sorted.length);
+
+        Arrays.sort(k1, comparator);
+        Arrays.sort(k2, comparator);
+
+        sorted = ArrayUtils.addAll(k2, k1);
         return sorted;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sortunits/SortUnitsTest.java
+++ chapter_003/src/test/java/ru/job4j/sortunits/SortUnitsTest.java
@@ -2,31 +2,64 @@ package ru.job4j.sortunits;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Класс тестов класса сортировки подразделений.
+ */
 public class SortUnitsTest {
+    /**
+     * Объект класса.
+     */
+    private SortUnits sortUnits = new SortUnits();
+    /**
+     * Несортированный массив без некоторых явно указанных подразделений.
+     */
+    private String[] units = new String[]{
+                                            "K1\\SK1",
+                                            "K1\\SK2",
+                                            "K1\\SK1\\SSK1",
+                                            "K1\\SK1\\SSK2",
+                                            "K2",
+                                            "K2\\SK1\\SSK1",
+                                            "K2\\SK1\\SSK2"};
 
-    SortUnits sortUnits = new SortUnits();
-
-    String[] units = new String[]{  "K1\\SK1",
-                                    "K1\\SK2",
-                                    "K1\\SK1\\SSK1",
-                                    "K1\\SK1\\SSK2",
-                                    "K2",
-                                    "K2\\SK1\\SSK1",
-                                    "K2\\SK1\\SSK2"};
-
-    String[] expected = new String[]{ "K1",
-                                      "K1\\SK1",
-                                      "K1\\SK1\\SSK1",
-                                      "K1\\SK1\\SSK2",
-                                      "K1\\SK2",
-                                      "K2",
-                                      "K2\\SK1",
-                                      "K2\\SK1\\SSK1",
-                                      "K2\\SK1\\SSK2"};
-
+    /**
+     * Тест сортировки по возрастанию.
+     */
     @Test
-    public void test1() {
-        sortUnits.parseUnits(units);
+    public void whenSortAscending() {
+        String[] expected = new String[]{
+                                            "K1",
+                                            "K1\\SK1",
+                                            "K1\\SK1\\SSK1",
+                                            "K1\\SK1\\SSK2",
+                                            "K1\\SK2",
+                                            "K2",
+                                            "K2\\SK1",
+                                            "K2\\SK1\\SSK1",
+                                            "K2\\SK1\\SSK2"};
+        String[] result = sortUnits.sortAscending(units);
+        assertThat(expected, is(result));
     }
 
+    /**
+     * Тест сортировки по убыванию.
+     */
+    @Test
+    public void whenSortDescendingly() {
+        String[] expected = new String[]{
+                                            "K2",
+                                            "K2\\SK1",
+                                            "K2\\SK1\\SSK2",
+                                            "K2\\SK1\\SSK1",
+                                            "K1",
+                                            "K1\\SK2",
+                                            "K1\\SK1",
+                                            "K1\\SK1\\SSK2",
+                                            "K1\\SK1\\SSK1"};
+        String[] result = sortUnits.sortDescendingly(units);
+        assertThat(expected, is(result));
+    }
 }
