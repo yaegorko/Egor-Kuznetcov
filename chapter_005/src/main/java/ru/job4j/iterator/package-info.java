1d3993f9ee90a7c138f0245a5788fa8a1d248d62
yaegorko <yaegorko@gmail.com>
2017-10-17 15:15:00 +0400

    5.1.1. Итератор для двухмерного массива int[][].

---
+++ chapter_005/src/main/java/ru/job4j/iterator/package-info.java
@@ -0,0 +1,8 @@
+/**.
+ Часть 005. Collections. Pro.
+ 1. Iterator
+@author - Egor K.
+@since - 17.10.17
+@version - $Id$
+*/
+package ru.job4j.iterator;
---
+++ chapter_005/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**.
+ Часть 005. Collections. Pro.
+@author - Egor K.
+@since - 17.10.17
+@version - $Id$
+*/
+package ru.job4j;
