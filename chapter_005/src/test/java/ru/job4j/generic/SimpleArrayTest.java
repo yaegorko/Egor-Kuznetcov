391a6bb412ddb450858561fc428d1d21fd4e98c6
yaegorko <yaegorko@gmail.com>
2017-10-25 20:34:35 +0400

    5.2.1. Реализовать SimpleArray<T> v1.0

--- chapter_005/src/test/java/ru/job4j/generic/SimpleArrayTest.java
+++ chapter_005/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -5,11 +5,16 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+/**
+ * Класс тестов класса SimpleArray.
+ */
 public class SimpleArrayTest {
-
+    /**
+     * Тест add.
+     */
     @Test
-    public void syntheticTest1() {
-        SimpleArray simpleArray =new SimpleArray(2);
+    public void whenAddElementsToSimpleArray() {
+        SimpleArray simpleArray = new SimpleArray(2);
         simpleArray.add(1);
         simpleArray.add("www");
         assertThat(simpleArray.get(0), is(1));
@@ -17,9 +22,26 @@ public class SimpleArrayTest {
 
     }
 
+    /**
+     * Тест update.
+     */
     @Test
-    public void syntheticTest2() {
-        SimpleArray simpleArray =new SimpleArray(5);
+    public void whenUpdateElementInSimpleArray() {
+        SimpleArray simpleArray = new SimpleArray(2);
+        simpleArray.add(1);
+        simpleArray.add("www");
+        simpleArray.update(0, "1");
+        assertThat(simpleArray.get(0), is("1"));
+        simpleArray.update(1, 2);
+        assertThat(simpleArray.get(1), is(2));
+    }
+
+    /**
+     * Тест delete.
+     */
+    @Test
+    public void whenDeleteElementFromSimpleArray() {
+        SimpleArray simpleArray = new SimpleArray<>(5);
         simpleArray.add(1);
         simpleArray.add(2);
         simpleArray.add(3);
@@ -27,7 +49,8 @@ public class SimpleArrayTest {
         simpleArray.add(5);
         simpleArray.delete(3);
         assertThat(simpleArray.get(3), is(5));
-        assertThat(simpleArray.get(4), is(0));
+        simpleArray.add(6);
+        assertThat(simpleArray.get(4), is(6));
 
     }
 
