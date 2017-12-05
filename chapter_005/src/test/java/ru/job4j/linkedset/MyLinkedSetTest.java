6ada7947154772ced4730a82ad029cc1951f4f21
yaegorko <yaegorko@gmail.com>
2017-12-05 16:02:08 +0400

    5.4.2 Set реализованный на связном списке. v1.2

--- chapter_005/src/test/java/ru/job4j/linkedset/MyLinkedSetTest.java
+++ chapter_005/src/test/java/ru/job4j/linkedset/MyLinkedSetTest.java
@@ -36,4 +36,25 @@ public class MyLinkedSetTest {
         iterator.next();
     }
 
+    /**
+     * Проверяем дубликаты на примитивных типах.
+     */
+    @Test
+    public void whenAddEqualElementsInList() {
+        this.myLinkedSet.add(1);
+        this.myLinkedSet.add(1);
+        this.myLinkedSet.add("2");
+        this.myLinkedSet.add("2");
+        this.myLinkedSet.add('@');
+        this.myLinkedSet.add('@');
+        Iterator iterator = myLinkedSet.iterator();
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(1));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("2"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is('@'));
+        assertThat(iterator.hasNext(), is(false));
+    }
+
 }
