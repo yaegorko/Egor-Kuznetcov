b4121a75d121681383f05ec4193b63c616752d91
yaegorko <yaegorko@gmail.com>
2017-12-28 15:36:25 +0400

    5.5.2. Не перекрывать equals, hashCode. v0.2

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -7,6 +7,12 @@ public class User {
     private int children;
     private Calendar birthday;
 
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
     public User() {
     }
 
--- chapter_005/src/test/java/ru/job4j/map/UserTest.java
+++ chapter_005/src/test/java/ru/job4j/map/UserTest.java
@@ -2,6 +2,7 @@ package ru.job4j.map;
 
 import org.junit.Test;
 
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,8 +10,8 @@ import static org.junit.Assert.*;
 
 public class UserTest {
 
-    User user1 = new User("Masha");
-    User user2 = new User("Masha");
+    User user1 = new User("Masha", 1, new GregorianCalendar(1975, GregorianCalendar.DECEMBER, 31));
+    User user2 = new User("Masha", 1, new GregorianCalendar(1975, GregorianCalendar.DECEMBER, 31));
 
     Map<User, Object> map = new HashMap();
 
