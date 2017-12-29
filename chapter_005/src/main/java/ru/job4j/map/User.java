0fc4eab32e33a45a2b800883ef913d511b3fd133
yaegorko <yaegorko@gmail.com>
2017-12-29 14:54:21 +0400

    5.5.3 Переопределил только equals.

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -14,8 +14,19 @@ public class User {
         this.birthday = birthday;
     }
 
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, children, birthday);
+//    }
+
+
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birthday, user.birthday);
     }
 }
